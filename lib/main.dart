3a8e6c35fe8aa51161b5348e5ded22002654ab40
jeseniaisabel231 <jesenia.pazto@epn.edu.ec>
2024-12-13 16:11:37 -0500

    estilos

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class HomeScreen extends StatelessWidget {
         title: Text('API Navigation Example'),
       ),
       body: Center(
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
